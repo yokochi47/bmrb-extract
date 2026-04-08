491f0bc05219609aa57ba69aa7c45d4202648add
Masashi Yokochi <yokochi47@gmail.com>
2026-04-08 04:35:58 +0000

    Add reload_wfe.sh script

---
+++ reload_wfe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# After running nginx, frontend is no longer needed.
+(sleep 60 ; [[ `docker compose ps frontend &> /dev/null` ]] && docker compose down frontend && yes | docker image prune) &
+
+docker compose down nginx
+docker compose build frontend
+docker compose build nginx
+docker compose up nginx -d --remove-orphans
+
