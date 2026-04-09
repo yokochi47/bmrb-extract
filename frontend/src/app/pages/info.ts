84a45b3126bdbcb9bf1a57e822ba8d7b2dfa7c9a
Masashi Yokochi <yokochi47@gmail.com>
2026-04-09 11:13:22 +0000

    Revice info page

--- frontend/src/app/pages/info.ts
+++ frontend/src/app/pages/info.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 import { ONEDEP_URL, BMRBDEP_URL } from '../../site.config';
 import { TermsConsent } from './terms.consent';
@@ -7,7 +8,7 @@ import { TermsConsent } from './terms.consent';
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [CommonModule, TermsConsent],
+  imports: [CommonModule, RouterLink, TermsConsent],
   templateUrl: './info.html',
 })
 export class Info {
