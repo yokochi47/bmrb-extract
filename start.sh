75157b75577a1b767788cf4fed88fadf415f1a88
Masashi Yokochi <yokochi47@gmail.com>
2026-04-16 05:40:22 +0000

    Do not hard code of POSTGRES_USER

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 # After running nginx, frontend is no longer needed.
 (sleep 120 ; docker compose down frontend) &
 
-docker compose up -d
+docker compose up -d --remove-orphans
 
 # Prune unused images
-# docker image prune
+yes | docker image prune
 
 # Prune unused volumes
-# docker volume prune
+yes | docker volume prune
 
